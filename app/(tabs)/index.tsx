bda3fe9dc12596a765a390dafbb350de905ef7a1
Henrique Casagrande [ Zallpy ] <henrique.casagrande@zallpy.com>
2024-02-08 17:14:49 -0300

    menu

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,36 +1,68 @@
-import { StyleSheet } from "react-native";
+import { ScrollView, StyleSheet } from "react-native";
 
 import BrandInfos from "../../barber.config.json";
-import EditScreenInfo from "../../components/EditScreenInfo";
 import { Text, View } from "../../components/Themed";
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
-    justifyContent: "center",
   },
   title: {
-    fontSize: 20,
+    fontSize: 17,
     fontWeight: "bold",
+    marginTop: 30,
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: "80%",
+  brandName: {
+    fontSize: 20,
+  },
+  employerContainer: {
+    flex: 1,
+    marginTop: 35,
+    backgroundColor: "green",
+  },
+  employersWrapper: { flexGrow: 1 },
+  employers: {
+    backgroundColor: "white",
+    padding: 8,
+  },
+  employerText: {
+    color: "red",
   },
 });
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>{BrandInfos.name}</Text>
-      <View
+      <Text style={styles.title}>
+        Bem Vindo a <Text style={styles.brandName}>{BrandInfos.name}</Text>
+      </Text>
+      {/* <View
         style={styles.separator}
         lightColor="#eee"
         darkColor="rgba(255,255,255,0.1)"
-      />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      /> */}
+
+      <ScrollView
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        style={styles.employersWrapper}
+      >
+        {BrandInfos.EmployerNames.map((item) => {
+          return (
+            <View style={styles.employers}>
+              {/* <Image
+                source={{
+                  uri: "https://reactnative.dev/img/tiny_logo.png",
+                }}
+              /> */}
+              <Text style={styles.employerText}>
+                {item.name} • {item.especialidade}
+              </Text>
+            </View>
+          );
+        })}
+      </ScrollView>
     </View>
   );
 }
